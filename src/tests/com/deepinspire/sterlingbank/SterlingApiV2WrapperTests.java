84704b320430f771332be50cd707b28801348142
Oleg Radchenko <oleg@deepinspire.com>
2019-11-01 19:14:41 +0200

    testing "com.fasterxml.jackson" package

--- src/tests/com/deepinspire/sterlingbank/SterlingApiV2WrapperTests.java
+++ src/tests/com/deepinspire/sterlingbank/SterlingApiV2WrapperTests.java
@@ -12,6 +12,9 @@ import java.text.SimpleDateFormat;
 import java.util.*;
 
 import static org.assertj.core.api.Assertions.assertThat;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.*;
 
 import main.com.deepinspire.sterlingbank.StarlingClient;
@@ -31,9 +34,6 @@ public class SterlingApiV2WrapperTests {
         try {
             Properties properties = new Properties();
             properties.load(new FileInputStream("./cfg/sandbox.properties"));
-
-            this.accountUid = properties.getProperty("starling.account.account-uid");
-            this.categoryUid = properties.getProperty("starling.account.category-uid");
 
             // SET HTTPS PROXY
             if (Boolean.parseBoolean(properties.getProperty("starling.proxy.enabled"))) {
@@ -84,6 +84,20 @@ public class SterlingApiV2WrapperTests {
                 properties.getProperty("starling.signing.public-key-uid"),
                 properties.getProperty("starling.access.token")
             );
+
+            this.accountUid = properties.getProperty("starling.account.account-uid");
+            this.categoryUid = properties.getProperty("starling.account.category-uid");
+
+            /*
+                Response response = client.getAccount();
+
+                ObjectMapper objectMapper = new ObjectMapper();
+                JsonNode jsonNode = objectMapper.readTree(response.getContent());
+
+                this.accountUid = jsonNode.get("accounts").get(0).get("accountUid").asText();
+                this.categoryUid = jsonNode.get("accounts").get(0).get("defaultCategory").asText();
+            */
+
         } catch (NoSuchAlgorithmException e) {
             e.printStackTrace();
         } catch (IOException e) {
